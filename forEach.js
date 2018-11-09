1ad474909b03b413a8abf485d06c9c6452c76cf1
rashadulalam <rashadul91@gmail.com>
2018-11-09 17:15:21 +0600

    ES6 style forEach iteration

--- forEach.js
+++ forEach.js
@@ -6,3 +6,9 @@ const fruits = ['mango', 'apple', 'pineapple', 'apple'];
 fruits.forEach(function(fruit){
 	console.log('I love ' + fruit);
 });
+
+
+
+
+// ES6 style
+fruits.forEach(fruit => console.log('I love to eat ' + fruit));
