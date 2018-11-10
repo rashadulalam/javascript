499685e3659f67ddf8bb7e5c2f0bdb46b36ca454
rashadulalam <rashadul91@gmail.com>
2018-11-10 07:33:56 +0600

    bigNumbers array created from numbers array

--- map.js
+++ map.js
@@ -1,3 +1,10 @@
 // .map is called on an array, it takes an argument of ca callback function and returns a new array
 
+const numbers = [1, 2, 3, 4, 5];
 
+const bigNumbers = numbers.map(number => {
+	return number * 10;
+});
+
+console.log(numbers);
+console.log(bigNumbers);
